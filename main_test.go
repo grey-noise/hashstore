c958a8a961689ba1f7f909c9879b284dddcd3c7d
Patrick Alin <patrick.alin@smals.be>
2017-08-30 21:49:56 +0200

    v1.6

--- main_test.go
+++ main_test.go
@@ -1,8 +1,37 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
 	"testing"
 )
 
-func TestMD5All(t *testing.T) {
+var out io.Writer = os.Stdout
+
+func TestListRuns(t *testing.T) {
+
+	buf := &bytes.Buffer{}
+	out = buf
+	dbname = "hashstore.db"
+	listRuns(nil)
+	fmt.Printf("ici %s", buf.String())
+
+	/*
+		tests := []struct {
+			name   string
+			fields Bloomsky
+			want   string
+		}{
+			{"Test1", mybloomskyTest1, "Thuin"},
+			{"Test2", mybloomskyTest2, "Paris"},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				if got := tt.fields.listRuns(nil); got != tt.want {
+					t.Errorf("BloomskyStructure.GetCity() = %v, want %v", got, tt.want)
+				}
+			})
+		}*/
 }
